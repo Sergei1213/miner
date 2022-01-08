7d0e2153d16a60fd9adf2a2cdcd0ac67bcbdf4aa
Sergei1213 <89149613+Sergei1213@users.noreply.github.com>
2022-01-08 12:34:19 +0200

    Delete miner.sh

--- miner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
-tar xvzf xmrig-6.16.2-linux-x64.tar.gz
-cd xmrig-6.16.2-linux-x64
-./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17056 --tls --user RTfF9FYUaz5Qx1psP1dc8vTrLMmCeuKzmA.1
